e716d598a9e36f1e83ef7f9bbb00927ff96d4195
Ikrom <iikromjon875@gmail.com>
2022-12-15 22:35:37 +0500

    world_map emoji

--- solution.go
+++ solution.go
@@ -1,9 +1,8 @@
 package solution
 
-import (
-	"github.com/kyokomi/emoji/v2"
-)
+import "github.com/kyokomi/emoji/v2"
 
 func GetMessage() string {
-	return emoji.Sprint("Hello :world_map:")
+	greeting := emoji.Sprint("Hello :world_map:")
+	return greeting
 }
