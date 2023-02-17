6d7b69ee120e347443fe96684f1ed39e463f6bf0
Ikrom <iikromjon875@gmail.com>
2023-02-17 16:04:56 +0500

    user, oop, struct, interface

---
+++ s01.go
@@ -0,0 +1,50 @@
+package structs
+
+import "reflect"
+
+type User struct {
+	lastName  string
+	firstName string
+}
+
+func New() User {
+	return User{}
+}
+
+type UserInterface interface {
+	SetFirstName(string)
+	SetLastName(string)
+	FullName() string
+}
+
+func (u User) SetFirstName(s string) {
+	u.firstName = s
+}
+
+func (u User) SetLastName(s string) {
+	u.lastName = s
+}
+
+func (u User) FullName() string {
+	return u.lastName + " " + u.firstName
+}
+
+func ResetUser(user User) {
+	user.SetLastName("")
+	user.SetFirstName("")
+}
+
+func IsUser(input interface{}) bool {
+	if reflect.TypeOf(input) == reflect.TypeOf(User{}) {
+		return true
+	}
+	return false
+}
+
+func ProcessUser(user UserInterface) string {
+	fName := "qq"
+	lName := "ww"
+	user.SetFirstName(fName)
+	user.SetLastName(lName)
+	return user.FullName()
+}
